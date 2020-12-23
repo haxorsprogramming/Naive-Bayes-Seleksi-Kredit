16206ecbe95a86127bb0fe2db75f2281f57de854
haxorsprogramming <haxorsuinsu@gmail.com>
2020-12-23 11:37:33 +0700

    update

---
+++ app/Http/Controllers/LoginCtr.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Import namespace & library
+ */
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+/**
+ * Import model
+ */
+
+
+class LoginCtr extends Controller
+{
+    public function loginpage()
+    {
+        
+    }
+}
